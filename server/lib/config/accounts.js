2d4182ba31b95306bd47882ac395a00ab3c6ad7f
Luca Mussi <luca.mussi@henesis.eu>
2014-11-25 11:48:29 +0100

    clean and update

--- server/lib/config/accounts.js
+++ server/lib/config/accounts.js
@@ -2,27 +2,8 @@
 Meteor.startup(function() {
     // Remove configuration entries in case service is already configured
     ServiceConfiguration.configurations.remove({$or: [
-        {service: "facebook"},
-        {service: "github"},
         {service: "google"},
-        {service: "linkedin"}
     ]});
-
-    /*
-    // Add Facebook configuration entry
-    ServiceConfiguration.configurations.insert({
-        "service": "facebook",
-        "appId": "XXXXXXXXXXXXXXX",
-        "secret": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-    });
-
-    // Add GitHub configuration entry
-    ServiceConfiguration.configurations.insert({
-        "service": "github",
-        "clientId": "XXXXXXXXXXXXXXXXXXXX",
-        "secret": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-    });
-    */
 
     // Add Google configuration entry
     ServiceConfiguration.configurations.insert({
@@ -31,13 +12,4 @@ Meteor.startup(function() {
         "client_email": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
         "secret": "XXXXXXXXXXXXXXXXXXXXXXXX"
     });
-
-    /*
-    // Add Linkedin configuration entry
-    ServiceConfiguration.configurations.insert({
-        "service": "linkedin",
-        "clientId": "XXXXXXXXXXXXXX",
-        "secret": "XXXXXXXXXXXXXXXX"
-    });
-    */
 });
